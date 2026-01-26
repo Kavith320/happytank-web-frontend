b4700b15403d9078a165eb5913f09bda35881940
Kavith320 <kavith.b.320udapola@gmail.com>
2026-01-26 17:32:48 +0530

    Initial frontend with auth, signup, and Google login UI

--- src/app/logout/page.jsx
+++ src/app/logout/page.jsx
@@ -2,13 +2,14 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { removeToken } from "../lib/auth";
+import { removeToken, removeUser } from "../lib/auth";
 
 export default function LogoutPage() {
   const router = useRouter();
 
   useEffect(() => {
     removeToken();
+    removeUser();
     router.replace("/login");
   }, [router]);
 
